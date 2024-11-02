81e35f12f42c4861bcfdc2059864c19f4de246e9
d0ublew <66501624+d0UBleW@users.noreply.github.com>
2024-11-02 15:11:25 +0700

    fix: yazi smart-enter plugin

--- modules/hm/yazi/default.nix
+++ modules/hm/yazi/default.nix
@@ -22,6 +22,14 @@ in
   };
 
   config = mkIf cfg.enable {
+    xdg.configFile."yazi/plugins/smart-enter.yazi/init.lua".text = ''
+      return {
+        entry = function()
+          local h = cx.active.current.hovered
+          ya.manager_emit(h and h.cha.is_dir and "enter" or "open", { hovered = true })
+        end,
+      }
+    '';
     programs.yazi = {
       enable = true;
       enableBashIntegration = true;

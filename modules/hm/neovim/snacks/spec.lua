6246499bab84263401972f1def8a5b3068cee9de
williamwijaya <66501624+d0UBleW@users.noreply.github.com>
2025-04-14 11:56:06 +0700

    feat(nvim): snacks terminal

--- modules/hm/neovim/snacks/spec.lua
+++ modules/hm/neovim/snacks/spec.lua
@@ -1,3 +1,26 @@
+local transparent = function()
+  vim.api.nvim_set_hl(0, "SnacksNormal", { bg = "NONE" })
+  vim.api.nvim_set_hl(0, "SnacksNormalNC", { bg = "NONE" })
+end
+
+local aug = vim.api.nvim_create_augroup("d0ublew_snacks", { clear = true })
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  callback = function()
+    transparent()
+  end,
+  pattern = "snacks_terminal",
+  group = aug,
+})
+
+vim.api.nvim_create_autocmd({ "ColorScheme" }, {
+  pattern = "*",
+  callback = function()
+    transparent()
+  end,
+  group = aug,
+})
+
 return {
   {
     "folke/snacks.nvim",
@@ -20,10 +43,57 @@ return {
       },
       quickfile = { enabled = true },
       input = { enabled = true, relative = "cursor" },
-      picker = { enabled = true },
+      picker = {
+        enabled = true,
+        -- ui_select = true
+      },
+      terminal = {
+        enabled = true,
+        bo = {
+          filetype = "snacks_terminal",
+        },
+        wo = {},
+        keys = {
+          q = "hide",
+          gf = function(self)
+            local f = vim.fn.findfile(vim.fn.expand("<cfile>"), "**")
+            if f == "" then
+              vim.notify("No file under cursor", vim.log.levels.WARN)
+            else
+              self:hide()
+              vim.schedule(function()
+                vim.cmd("e " .. f)
+              end)
+            end
+          end,
+          term_normal = {
+            "<esc>",
+            function(self)
+              self.esc_timer = self.esc_timer or (vim.uv or vim.loop).new_timer()
+              if self.esc_timer:is_active() then
+                self.esc_timer:stop()
+                vim.cmd("stopinsert")
+              else
+                self.esc_timer:start(200, 0, function() end)
+                return "<esc>"
+              end
+            end,
+            mode = "t",
+            expr = true,
+            desc = "Double escape to normal mode",
+          },
+        },
+      },
     },
     keys = {
       { "<leader>z", "<cmd>lua Snacks.zen()<CR>", "Toggle zen mode" },
+      {
+        "<leader><space>",
+        function()
+          Snacks.terminal(nil, { start_insert = false, auto_insert = false })
+        end,
+        "Snacks terminal",
+      },
     },
   },
 }
